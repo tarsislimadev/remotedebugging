9e8cf2383f748c6610cd3c227864a0569f41b140
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-02-23 21:53:08 -0300

    Add some methods in list

--- menu.js
+++ menu.js
@@ -3,21 +3,88 @@ const menu = [
     'Page.navigate',
     async ({ req, question } = {}) => {
       const answer = await question('Enter URL to navigate to:');
-      req.push({ method: 'Page.navigate', params: { url: answer } });
+      req.push({ 'method': 'Page.navigate', params: { url: answer } });
+    }
+  ],
+  ['Page.reload', async ({ ws } = {}) => ws.send({ 'method': 'Page.reload', 'params': { 'ignoreCache': true } })],
+  ['Page.captureScreenshot', async ({ ws } = {}) => ws.send({ 'method': 'Page.captureScreenshot', 'params': { 'format': 'png' } })],
+  [
+    'Page.addScriptToEvaluateOnNewDocument',
+    async ({ req, question } = {}) => {
+      const source = await question('Source:');
+      req.push({ 'method': 'Page.addScriptToEvaluateOnNewDocument', params: { source } });
+    }
+  ],
+  ['Page.bringToFront', async ({ ws }) => ws.send({ 'method': 'Page.bringToFront' })],
+  [
+    'Page.getAppManifest',
+    async ({ ws, question }) => {
+      const manifestId = await question('Manifest Id:');
+      ws.send({ 'method': 'Page.getAppManifest', params: { manifestId } })
+    }
+  ],
+  ['Page.getFrameTree', async ({ ws }) => ws.send({ 'method': 'Page.getFrameTree' })],
+  ['Page.getLayoutMetrics', async ({ ws }) => ws.send({ 'method': 'Page.getLayoutMetrics' })],
+  ['Page.getNavigationHistory', async ({ ws }) => ws.send({ 'method': 'Page.getNavigationHistory' })],
+  [
+    'Page.handleJavaScriptDialog',
+    async ({ ws, question }) => {
+      const accept = await question('Accept (0/1):');
+      const promptText = await question('Prompt Text:');
+      req.push({ 'method': 'Page.handleJavaScriptDialog', 'params': { accept, promptText } });
     }
   ],
   [
-    'Page.reload',
-    async ({ ws } = {}) => {
-      ws.send({ 'method': 'Page.reload', 'params': { 'ignoreCache': true } })
+    'Page.navigateToHistoryEntry',
+    async ({ req, question }) => {
+      const entryId = await question('Entry Id:');
+      req.push({ 'method': 'Page.navigateToHistoryEntry', 'params': { entryId } });
     }
   ],
   [
-    'Page.captureScreenshot',
-    async ({ ws } = {}) => {
-      ws.send({ 'method': 'Page.captureScreenshot', 'params': { 'format': 'png' } })
+    'Page.printToPDF',
+    async ({ req, question }) => {
+      const landscape = await question('landscape (0/1):');
+      const displayHeaderFooter = await question('display header and footer (0/1):');
+      const printBackground = await question('print background (0/1):');
+      const scale = await question('scale (0/1):');
+      req.push({ 'method': 'Page.printToPDF', 'params': { landscape, displayHeaderFooter, printBackground, scale } });
     }
   ],
+  [
+    'Page.setBypassCSP',
+    async ({ req, question }) => {
+      const enabled = await question('enabled (0/1):')
+      req.push({ 'method': 'Page.setBypassCSP', 'params': { 'enabled': !!enabled } });
+    }
+  ],
+  [
+    'Page.setDocumentContent',
+    async ({ req, question }) => {
+      const frameId = await question('frame id:')
+      const html = await question('html:')
+      req.push({ 'method': 'Page.setDocumentContent', 'params': { frameId, html } })
+    }
+  ],
+  [
+    'Page.setInterceptFileChooserDialog',
+    async ({ req, question }) => {
+      const enabled = await question('enabled (0/1):')
+      req.push({ 'method': 'Page.setInterceptFileChooserDialog', 'params': { enabled } })
+    }
+  ],
+  [
+    'Page.setLifecycleEventsEnabled',
+    async ({ req, question }) => {
+      const enabled = await question('enabled (0/1):')
+      req.push({ 'method': 'Page.setLifecycleEventsEnabled', 'params': { enabled } })
+    }
+  ],
+  ['Page.resetNavigationHistory', async ({ ws }) => ws.send({ 'method': 'Page.resetNavigationHistory', 'params': {} })],
+  ['Page.stopLoading', async ({ ws }) => ws.send({ 'method': 'Page.stopLoading', 'params': {} })],
+  ['Page.disable', async ({ ws }) => ws.send({ 'method': 'Page.disable', 'params': {} })],
+  ['Page.close', async ({ ws }) => ws.send({ 'method': 'Page.close', 'params': {} })],
+  //
   [
     'Runtime.evaluate',
     async ({ req, question } = {}) => {
@@ -48,27 +115,6 @@ const menu = [
 ]
 
 module.exports = { menu }
-
-// ['Page.addScriptToEvaluateOnNewDocument', async () => {}],
-// ['Page.bringToFront', async () => {}],
-// ['Page.close', async () => {}],
-// ['Page.createIsolatedWorld', async () => {}],
-// ['Page.disable', async () => {}],
-// ['Page.enable', async () => {}],
-// ['Page.getAppManifest', async () => {}],
-// ['Page.getFrameTree', async () => {}],
-// ['Page.getLayoutMetrics', async () => {}],
-// ['Page.getNavigationHistory', async () => {}],
-// ['Page.handleJavaScriptDialog', async () => {}],
-// ['Page.navigateToHistoryEntry', async () => {}],
-// ['Page.printToPDF', async () => {}],
-// ['Page.removeScriptToEvaluateOnNewDocument', async () => {}],
-// ['Page.resetNavigationHistory', async () => {}],
-// ['Page.setBypassCSP', async () => {}],
-// ['Page.setDocumentContent', async () => {}],
-// ['Page.setInterceptFileChooserDialog', async () => {}],
-// ['Page.setLifecycleEventsEnabled', async () => {}],
-// ['Page.stopLoading', async () => {}],
 
 // ['Runtime.addBinding', async () => {}],
 // ['Runtime.awaitPromise', async () => {}],
